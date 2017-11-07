7ed417a33d8c9a4a5e301a54c6bdafbbf80fc4ea
pansophism <pansophism@gmail.com>
2017-11-07 21:40:41 +0000

    commiting 338.counting-bits.java

--- 338.counting-bits.java
+++ 338.counting-bits.java
@@ -4,9 +4,8 @@ class Solution {
         int [] dp = new int[num + 1];
 
         dp[0] = 0;
-        dp[1] = 1;
 
-        for(int i = 2; i <= num; i++) {
+        for(int i = 1; i <= num; i++) {
             dp[i] = dp[i / 2] + (i & 1);
         }
 
