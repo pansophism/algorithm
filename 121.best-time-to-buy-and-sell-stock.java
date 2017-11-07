4f65d48cdca1a648cd35845172b107a6c9880f7f
pansophism <pansophism@gmail.com>
2017-11-07 06:21:41 +0000

    commiting 121.best-time-to-buy-and-sell-stock.java

---
+++ 121.best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,17 @@
+class Solution {
+
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length == 0) {
+            return 0;
+        }
+
+        int minSoFar = prices[0], max = -1;
+
+        for(int i = 1; i < prices.length; i++) {
+            max = Math.max(max, prices[i] - minSoFar);
+            minSoFar = Math.min(minSoFar, prices[i]);
+        }
+
+        return max;
+    }
+}
