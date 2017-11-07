4c985aa4440db6752314c3c3c1ece6d615a86fb4
pansophism <pansophism@gmail.com>
2017-11-07 06:31:59 +0000

    commiting 198.house-robber.java

--- 198.house-robber.java
+++ 198.house-robber.java
@@ -12,7 +12,7 @@ class Solution {
         dp[1] = Math.max(nums[0], nums[1]);
 
         for(int i = 2; i < dp.length; i++) {
-            dp[i] = Math.max(nums[i - 2] + nums[i], nums[i - 1]);
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
         }
 
         return dp[nums.length - 1];
