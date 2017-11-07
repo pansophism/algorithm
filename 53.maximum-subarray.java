9f3411c607c00388cb660ab618a4491f3049c4b8
pansophism <pansophism@gmail.com>
2017-11-07 06:18:04 +0000

    commiting 53.maximum-subarray.java

---
+++ 53.maximum-subarray.java
@@ -0,0 +1,17 @@
+class Solution {
+
+    public int maxSubArray(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int sum = nums[0], max = nums[0];
+
+        for(int i = 1; i < nums.length; i++) {
+            sum = Math.max(sum + nums[i], nums[i]);
+            max = Math.max(sum, max);
+        }
+
+        return max;
+    }
+}
