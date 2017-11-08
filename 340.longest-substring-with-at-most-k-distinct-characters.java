74e3452ea4bde2ab45977d6554cfc431fcee33bc
pansophism <pansophism@gmail.com>
2017-11-08 09:00:44 +0000

    commiting 340.longest-substring-with-at-most-k-distinct-characters.java

---
+++ 340.longest-substring-with-at-most-k-distinct-characters.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+
+        char [] dict = new char[256];
+        int count = 0, maxLen = 0;
+
+        char [] arr = s.toCharArray();
+
+        int i = 0, j = 0;
+
+        while(j < arr.length) {
+            if(count == k) {
+                maxLen = Math.max(maxLen, j - i + 1);
+            }
+
+            while(count >= k) {
+                dict[arr[i]]--;
+                if(dict[arr[i]] == 0) {
+                    count--;
+                }
+                i++;
+            }
+
+            if(dict[arr[j]] == 0) {
+                count++;
+            }
+            dict[arr[j]]++;
+            j++;
+        }
+
+        return maxLen;
+    }
+}
