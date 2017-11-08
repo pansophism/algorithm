65500b7ced083d18efb30570cdd4006f872ffccc
pansophism <pansophism@gmail.com>
2017-11-08 08:17:14 +0000

    commiting 10.regular-expression-matching.java

--- 10.regular-expression-matching.java
+++ 10.regular-expression-matching.java
@@ -8,30 +8,30 @@ class Solution {
         char [] sArr = s.toCharArray();
         char [] pArr = p.toCharArray();
 
-        boolean [][] dp = new boolean[sArr.length + 1][pArr.length + 1];
-        dp[0][0] = true;
+        boolean [][] space = new boolean[sArr.length + 1][pArr.length + 1];
+        space[0][0] = true;
 
-        for(int i = 3; i < p.length(); i++) {
-            if(pArr[i] == '*') {
-                dp[i][0] = dp[i - 2][0];
+        for(int i = 2; i <= pArr.length; i++) {
+            if(pArr[i - 1] == '*') {
+                space[0][i] = space[0][i - 2];
             }
         }
 
         for(int i = 1; i <= sArr.length; i++) {
             for(int j = 1; j <= pArr.length; j++) {
-                if(sArr[i - 1] == pArr[j - 1] || pArr[j - 1] == '.') {
-                    dp[i][j] = dp[i - 1][j - 1];
+                if(sArr[i - 1] == pArr[j - 1]) {
+                    space[i][j] = space[i - 1][j - 1];
                 } else if(pArr[j - 1] == '*') {
                     if(sArr[i - 1] == pArr[j - 2] || pArr[j - 2] == '.') {
-                        dp[i][j] = dp[i - 2][j - 1];
+                        space[i][j] = space[i - 1][j] || space[i][j - 1] || space[i][j - 2];
                     } else {
-                        dp[i][j] = false;
+                        space[i][j] = space[i][j - 2];
                     }
                 }
             }
         }
 
-        return dp[sArr.length][pArr.length];
+        return space[sArr.length][pArr.length];
 
     }
 }
