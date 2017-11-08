8357d361ec961c1221c9f480eb8c0e0498b6c2f0
pansophism <pansophism@gmail.com>
2017-11-08 03:06:19 +0000

    commiting 188.best-time-to-buy-and-sell-stock-iv.java

--- 188.best-time-to-buy-and-sell-stock-iv.java
+++ 188.best-time-to-buy-and-sell-stock-iv.java
@@ -2,8 +2,19 @@ class Solution {
 
     public int maxProfit(int k, int[] prices) {
 
-        if(prices == null || prices.length == 0 || k <= 0) {
+        if(prices == null || prices.length <= 1 || k <= 0) {
             return 0;
+        }
+
+        if(k >= prices.length / 2) {
+            int maxPro = 0;
+            for(int i = 1; i < prices.length; i++) {
+                if(prices[i] > prices[i - 1]) {
+                    maxPro += prices[i] - prices[i - 1];
+                }
+            }
+
+            return maxPro;
         }
 
         int [][] space = new int[k + 1][prices.length];
