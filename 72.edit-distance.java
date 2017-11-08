db8d0643d885488b851f7ed6c0551b03316631c7
pansophism <pansophism@gmail.com>
2017-11-08 01:27:47 +0000

    commiting 72.edit-distance.java

---
+++ 72.edit-distance.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        if(word1 == null || word2 == null) {
+            return false;
+        }
+
+        char [] arr1 = word1.toCharArray();
+        char [] arr2 = word1.toCharArray();
+
+        int [][] space = new int[arr1.length + 1][arr2.length];
+
+        for(int i = 1; i <= arr1.length; i++) {
+            for(int j = 1; j <= arr2.length; j++) {
+                if(arr1[i - 1] == arr2[j - 1]) {
+                    space[i][j] = space[i - 1][j - 1];
+                } else {
+                    space[i][j] = Math.max(
+                            Math.max(space[i - 1][j], space[i][j - 1]),
+                            space[i - 1][j - 1]) + 1;
+                }
+            }
+        }
+
+        return space[arr1.length][arr2.length];
+    }
+}
