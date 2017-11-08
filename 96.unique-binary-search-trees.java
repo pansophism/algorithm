f8b9bcbec50dcc2bc45b774648651a97c66ffe06
pansophism <pansophism@gmail.com>
2017-11-08 01:44:58 +0000

    commiting 96.unique-binary-search-trees.java

--- 96.unique-binary-search-trees.java
+++ 96.unique-binary-search-trees.java
@@ -1,18 +1,25 @@
 class Solution {
 
     public int numTrees(int n) {
-
-        int [] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
-
-        for(int i = 2; i <= n; i++) {
-            dp[i] = 0;
-            for(int j = 1; j <= i; j++) {
-                dp[i] += (dp[i - j] * dp[j - 1]);
-            }
+        if(n <= 3) {
+            return n;
         }
 
-        return dp[n];
+        int [] space = new int[n + 1];
+        space[0] = 0;
+        space[1] = 1;
+
+        for(int i = 2; i <= n; i++) {
+            int sum = 0;
+
+            for(int j = 1; j < i; j++) {
+                sum += space[j] * space[i - j];
+            }
+
+            space[i] = sum;
+
+        }
+
+        return space[n];
     }
 }
