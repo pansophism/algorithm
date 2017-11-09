f4398db384b255ca8bcdf8ff8147ad3c8636c20e
pansophism <pansophism@gmail.com>
2017-11-09 08:22:31 +0000

    commiting 139.word-break.java

--- 139.word-break.java
+++ 139.word-break.java
@@ -2,18 +2,21 @@ class Solution {
 
     public boolean wordBreak(String s, List<String> wordDict) {
 
-        if(s.length() == 0) {
-            return true;
-        }
+        Set<String> dict = new HashSet<String>(wordDict);
 
-        for(String w : wordDict) {
-            // System.out.println("string : " + s + " word : " + w);
-            if(s.startsWith(w)) {
-                return wordBreak(s.substring(w.length()), wordDict);
+        boolean [] arr = new boolean[s.length() + 1];
+        arr[0] = true;
+
+        for(int i = 1; i <= s.length(); i++) {
+            for(int j = 0; j < i; j++) {
+                if(arr[j] && dict.contains(s.substring(j, i)) {
+                    arr[i] = true;
+                    break;
+                }
             }
         }
 
-        return false;
+        return arr[s.length()];
 
     }
 }
