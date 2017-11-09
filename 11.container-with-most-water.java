8adab08155cadf1b42d3b312ecb3cd0e7e39a7c3
pansophism <pansophism@gmail.com>
2017-11-09 07:22:16 +0000

    commiting 11.container-with-most-water.java

--- 11.container-with-most-water.java
+++ 11.container-with-most-water.java
@@ -1,5 +1,15 @@
 class Solution {
     public int maxArea(int[] height) {
-        
+        int left = 0, right = height.length - 1, max = 0;
+
+        while(left < right) {
+            max = Math.max(max, (right - left) * Math.min(height[right], height[left]));
+            if(height[right] > height[left]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return max;
     }
 }
