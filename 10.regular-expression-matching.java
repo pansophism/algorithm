618f6b763d310944deb181b3fd36720bcb75a949
pansophism <pansophism@gmail.com>
2017-11-09 07:16:53 +0000

    commiting 10.regular-expression-matching.java

--- 10.regular-expression-matching.java
+++ 10.regular-expression-matching.java
@@ -11,15 +11,15 @@ class Solution {
         boolean [][] dp = new boolean[sArr.length + 1][pArr.length + 1];
         dp[0][0] = true;
 
-        for(int i = 2; i < pArr.length; i++) {
-            if(pArr[i] == '*') {
+        for(int i = 2; i <= pArr.length; i++) {
+            if(pArr[i - 1] == '*') {
                 dp[0][i] = dp[0][i - 2];
             }
         }
 
         for(int i = 1; i <= sArr.length; i++) {
             for(int j = 1; j <= pArr.length; j++) {
-                if(sArr[i - 1] == pArr[j - 1]) {
+                if(sArr[i - 1] == pArr[j - 1] || pArr[j - 1] == '.') {
                     dp[i][j] = dp[i - 1][j - 1];
                 } else {
                     if(pArr[j - 1] == '*') {
