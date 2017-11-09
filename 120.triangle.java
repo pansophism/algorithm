54f4b55f088170b244966eb3e8660d30da17ccd5
pansophism <pansophism@gmail.com>
2017-11-09 06:24:55 +0000

    commiting 120.triangle.java

---
+++ 120.triangle.java
@@ -0,0 +1,13 @@
+class Solution {
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int [] a = new int[triangle.size() + 1];
+
+        for(int i = triangle.size() - 1; i >= 0; i--) {
+            for(int j = 0; j < triangle.get(i).size(); j++) {
+                a[j] = Math.min(a[j], a[j + 1]) + triangle.get(i).get(j);
+            }
+        }
+        return a[0];
+    }
+}
