6270eb5427b6ea449f72928b99aa7f9ea376d396
pansophism <pansophism@gmail.com>
2017-11-09 08:04:08 +0000

    commiting 338.counting-bits.java

--- 338.counting-bits.java
+++ 338.counting-bits.java
@@ -1,15 +1,16 @@
 class Solution {
+
     public int[] countBits(int num) {
 
-        int [] dp = new int[num + 1];
+        int [] arr = new int[num + 1];
+        arr[0] = 0;
+        arr[1] = 1;
 
-        dp[0] = 0;
-
-        for(int i = 1; i <= num; i++) {
-            dp[i] = dp[i / 2] + (i & 1);
+        for(int i = 2; i <= num; i++) {
+            arr[i] = arr[i / 2] + (i & 1);
         }
 
-        return dp;
-
+        return arr;
     }
+
 }
