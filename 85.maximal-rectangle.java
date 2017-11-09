d659902f669caf9534b0e1c17291623014ad5e2f
pansophism <pansophism@gmail.com>
2017-11-09 09:08:54 +0000

    commiting 85.maximal-rectangle.java

--- 85.maximal-rectangle.java
+++ 85.maximal-rectangle.java
@@ -1,21 +1,17 @@
 class Solution {
 
     public int maximalRectangle(char[][] matrix) {
-
         if(matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return 0;
         }
 
         int [] hi = new int[matrix[0].length + 1];
-
         int max = 0;
 
         for(int r = 0; r < matrix.length; r++) {
-            char [] row = matrix[r];
-
-            for(int c = 0; c < row.length; c++) {
-                if(row[c] == '1') {
-                    hi[c] += 1;
+            for(int c = 0; c < matrix[0].length; c++) {
+                if(matrix[r][c] == '1') {
+                    hi[c]++;
                 } else {
                     hi[c] = 0;
                 }
@@ -23,17 +19,19 @@ class Solution {
 
             Stack<Integer> stack = new Stack<>();
 
-            for(int i = 0; i <= row.length; i++) {
+            for(int i = 0; i <= matrix[0].length; i++) {
                 if(stack.isEmpty() || hi[i] >= hi[stack.peek()]) {
                     stack.push(i);
-                } else {
+                } else{
                     while(!stack.isEmpty() && hi[stack.peek()] > hi[i]) {
                         int top = stack.pop();
                         int area = hi[top] * (stack.isEmpty() ? i : (i - stack.peek() - 1));
-                        max = Math.max(max, area);
+                        max = Math.max(area, max);
                     }
+
                     stack.push(i);
                 }
+
             }
 
         }
