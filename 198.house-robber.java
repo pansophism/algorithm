c8227ccbe183a7060dddd7644d19e6aff691a207
pansophism <pansophism@gmail.com>
2017-11-09 06:38:07 +0000

    commiting 198.house-robber.java

--- 198.house-robber.java
+++ 198.house-robber.java
@@ -3,18 +3,16 @@ class Solution {
     public int rob(int[] nums) {
         if(nums == null || nums.length == 0) {
             return 0;
-        } else if(nums.length == 1) {
-            return nums[0];
         }
 
-        int [] dp = new int[nums.length];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
+        int [] arr = new int[nums.length];
+        arr[0] = 0;
+        arr[1] = nums[1];
 
-        for(int i = 2; i < dp.length; i++) {
-            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        for(int i = 2; i < nums.length; i++) {
+            arr[i] = Math.max(arr[i - 2] + nums[i], arr[i - 1]);
         }
 
-        return dp[nums.length - 1];
+        return arr[nums.length - 1];
     }
 }
