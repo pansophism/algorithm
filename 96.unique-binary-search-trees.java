c5796761d7da5e22ee88aef6b4fb96f8d647674b
pansophism <pansophism@gmail.com>
2017-11-10 02:27:11 +0000

    test unique binary search trees

--- 96.unique-binary-search-trees.java
+++ 96.unique-binary-search-trees.java
@@ -9,10 +9,15 @@ class Solution {
         for(int i = 2; i <= n; i++) {
             space[i] = 0;
             for(int j = 1; j <= i; j++) {
+                System.out.println("i : " + i + " || i - j = " + (i - j) + " || j - 1 = " + (j - 1));
                 space[i] += (space[i - j] * space[j - 1]);
             }
         }
 
         return space[n];
+    }
+    public static void main(String [] args) {
+        Solution s = new Solution();
+        s.numTrees(5);
     }
 }
