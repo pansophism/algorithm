8ee1de70e5f57647b225525fd3f8f8cd9b1f89fe
pansophism <pansophism@gmail.com>
2017-11-10 02:40:55 +0000

    commiting 279.perfect-squares.java

--- 279.perfect-squares.java
+++ 279.perfect-squares.java
@@ -5,38 +5,20 @@ class Solution {
             return n;
         }
 
-        int [] space = new int[n + 1];
-        space[0] = 0;
-        space[1] = 1;
-        space[2] = 2;
-        space[3] = 3;
+        int [] arr = new int[n + 1];
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 2;
 
-        /**
-         * 1 -> 1
-         *
-         * 5 -> 1, 4
-         *
-         * 16 -> [0], 1, 4, 9, { 16 }
-         *
-         * 17 -> [0] 1, 4, 9, 16
-         *
-         **/
-        for(int i = 4; i <= n; i++) {
-
-            int t = Integer.MAX_VALUE;
+        for(int i = 3; i <= n; i++) {
+            int temp = Integer.MAX_VALUE;
             for(int j = 1; j * j <= i; j++) {
-                if(i == j * j) {
-                    t = 1;
-                } else {
-                    t = Math.min(t, space[i - j * j] + 1);
-                }
+                temp = Math.min(temp, arr[i - j * j] + 1);
             }
 
-            space[i] = t;
+            arr[i] = temp;
         }
 
-        return space[n];
-
-
+        return arr[n];
     }
 }
