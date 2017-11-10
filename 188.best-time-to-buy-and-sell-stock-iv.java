f4c56d28d3c62a01c953934a34d65d410f287052
pansophism <pansophism@gmail.com>
2017-11-10 03:05:45 +0000

    commiting 188.best-time-to-buy-and-sell-stock-iv.java

--- 188.best-time-to-buy-and-sell-stock-iv.java
+++ 188.best-time-to-buy-and-sell-stock-iv.java
@@ -2,36 +2,23 @@ class Solution {
 
     public int maxProfit(int k, int[] prices) {
 
-        if(prices == null || prices.length <= 1 || k <= 0) {
+        if(k <= 0 || prices == null || prices.length <= 1) {
             return 0;
         }
 
-        if(k >= prices.length / 2) {
-            int maxPro = 0;
-            for(int i = 1; i < prices.length; i++) {
-                if(prices[i] > prices[i - 1]) {
-                    maxPro += prices[i] - prices[i - 1];
-                }
-            }
-
-            return maxPro;
-        }
-
-        int [][] space = new int[k + 1][prices.length];
+        int [][] dp = new int[k + 1][prices.length];
 
         for(int i = 1; i <= k; i++) {
-
-            // buy at the first day how much i would make / invest
-            int pMax = 0 - prices[0];
-
-            for(int j = 1; j < prices.length; j++) {
-                space[i][j] = Math.max(space[i][j - 1], pMax + prices[j]);
-                // what if I buy / invest today
-                pMax = Math.max(pMax, space[i - 1][j] - prices[j]);
+            for(int j = 0; j < prices.length; j++) {
+                int temp = 0;
+                for(int m = 0; m < j; m++) {
+                    temp = Math.max(0, dp[i - 1][m] - prices[m] + prices[j]);
+                }
+                dp[i][j] = temp;
             }
         }
 
-        return space[k][prices.length - 1];
+        return dp[k][prices.length - 1];
 
     }
 }
