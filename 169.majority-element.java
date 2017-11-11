87eb2ba05f7cc2019a5db85fffbdf951939ead4c
pansophism <pansophism@gmail.com>
2017-11-11 22:13:51 +0000

    commiting 169.majority-element.java

--- 169.majority-element.java
+++ 169.majority-element.java
@@ -8,15 +8,16 @@ class Solution {
         }
 
         for(int i = 1; i < nums.length; i++) {
+
+            if(count < 0) {
+                candidate = nums[i];
+            }
             if(nums[i] == candidate) {
                 count++;
             } else {
                 count--;
             }
 
-            if(count < 0) {
-                candidate = nums[i];
-            }
         }
 
         return candidate;
