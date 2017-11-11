02ceabf934521cfe7e6904c2157ed866c90bae01
pansophism <pansophism@gmail.com>
2017-11-11 22:35:00 +0000

    commiting 53.maximum-subarray.java

--- 53.maximum-subarray.java
+++ 53.maximum-subarray.java
@@ -1,17 +1,39 @@
 class Solution {
 
     public int maxSubArray(int[] nums) {
-        if(nums == null || nums.length == 0) {
-            return 0;
+        int start = 0, end = nums.length;
+
+        return sumArray(nums, start, end - 1);
+    }
+
+    private int sumArray(int [] nums, int left, int right) {
+        if(left == right) {
+            return nums[left];
         }
 
-        int sum = nums[0], max = nums[0];
+        int mid = left + (right - left) / 2;
 
-        for(int i = 1; i < nums.length; i++) {
-            sum = Math.max(sum + nums[i], nums[i]);
-            max = Math.max(sum, max);
+        int leftSum = sumArray(nums, left, mid);
+        int rightSum = sumArray(nums, mid + 1, right);
+        int crossSum = crossingSum(nums, left, mid, right);
+
+        return Math.max(crossSum, Math.max(leftSum, rightSum));
+    }
+
+    private int crossingSum(int [] nums, int left, int mid, int right) {
+        int lSum = 0, lMax = Integer.MIN_VALUE;
+        for(int k = mid; k >= left; k--) {
+            lSum += nums[k];
+            lMax = Math.max(lMax, lSum);
         }
 
-        return max;
+        int rSum = 0, rMax = Integer.MIN_VALUE;
+        for(int k = mid + 1; k <= right; k++) {
+            rSum += nums[k];
+            rMax = Math.max(rMax, rSum);
+        }
+
+        return lMax + rMax;
     }
 }
+
