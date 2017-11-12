f6371c5825f7b91ac5e64e1fb6957599ca7d1551
pansophism <pansophism@gmail.com>
2017-11-12 18:47:15 +0000

    commiting 322.coin-change.java

--- 322.coin-change.java
+++ 322.coin-change.java
@@ -1,5 +1,28 @@
 class Solution {
+
     public int coinChange(int[] coins, int amount) {
+
+        if(amount <= 0) {
+            return 0;
+        }
+
+        int [] dp = new int[amout + 1];
+        int sum = 0;
+
+        while(++sum <= amount) {
+            int min = -1;
+            for(int coin : coins) {
+                if(sum >= coin && dp[sum-coin] != -1) {
+                    int temp = dp[sum-coin] + 1;
+                    min = min < 0 ? temp : (temp < min ? temp : min);
+                }
+
+            }
+            dp[sum] = min;
+
+        }
+        return dp[amount];
     }
+}
 }
 
