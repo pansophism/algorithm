dda63dd03c9b4315d3c87fc7afcdcfcea685ff88
pansophism <pansophism@gmail.com>
2017-11-28 09:12:46 -0800

    commiting 315.count-of-smaller-numbers-after-self.java

--- 315.count-of-smaller-numbers-after-self.java
+++ 315.count-of-smaller-numbers-after-self.java
@@ -17,9 +17,8 @@ class Solution {
         }
 
         Node root = new Node(nums[nums.length - 1]);
-        count[count.length - 1] = 0;
 
-        for(int i = nums.length - 2; i >= 0; i--) {
+        for(int i = nums.length - 1; i >= 0; i--) {
             count[i] = insert(root, nums[i]);
         }
 
