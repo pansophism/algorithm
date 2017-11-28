24b09279035a953d5a28b06c375bb6cfe725d17f
pansophism <pansophism@gmail.com>
2017-11-28 00:03:47 -0800

    commiting 315.count-of-smaller-numbers-after-self.java

--- 315.count-of-smaller-numbers-after-self.java
+++ 315.count-of-smaller-numbers-after-self.java
@@ -4,20 +4,26 @@ class Solution {
 
         int [] ans = new int[nums.length];
 
-        for(int i = nums.length - 1; i >= 0; i++) {
+        for(int i = nums.length - 1; i >= 0; i--) {
             if(i == nums.length - 1) {
                 ans[i] = 0;
             } else {
                 for(int j = i + 1; j < nums.length; j++) {
                     if(nums[j] < nums[i]) {
-                        ans[j] = ans[i] + 1;
+                        ans[i] = ans[j] + 1;
                         break;
                     }
                 }
             }
         }
 
-        return Arrays.asList(ans);
+        List<Integer> converted = new ArrayList<Integer>();
+
+        for(int i : ans) {
+            converted.add(i);
+        }
+
+        return converted;
     }
 
 }
