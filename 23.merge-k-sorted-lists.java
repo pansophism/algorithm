6fa97e50a136d75e3691992665b9fe402a1a3b76
pansophism <pansophism@gmail.com>
2017-12-01 15:00:46 -0800

    commiting 23.merge-k-sorted-lists.java

--- 23.merge-k-sorted-lists.java
+++ 23.merge-k-sorted-lists.java
@@ -2,37 +2,39 @@
 class Solution {
 
     public ListNode mergeKLists(ListNode[] lists) {
+
         return partition(lists, 0, lists.length - 1);
     }
 
-    public ListNode partition(ListNode [] lists, int start, int end) {
-        if(start == end) {
-            return lists[start];
+    public ListNode partition(ListNode[] lists, int lo, int hi) {
+        if(lo == hi) {
+            return lists[lo];
         }
 
-        if(start < end) {
-            int mid = start + (end - start) / 2;
+        if(lo < hi) {
+            int mid = lo + (hi - lo) / 2;
 
-            ListNode l1 = partition(lists, start, mid);
-            ListNode l2 = partition(lists, mid + 1, end);
+            ListNode l1 = partition(lists, lo, mid);
+            ListNode l2 = partition(lists, mid + 1, hi);
 
-            return mergeTwoLists(l1, l2);
+            return merge(l1, l2);
         }
 
+        // this should never happen
         return null;
     }
 
-    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l2 == null) return l1;
+    private ListNode merge(ListNode l1, ListNode l2) {
         if(l1 == null) return l2;
+        if(l2 == null) return l1;
 
-        if(l1.val < l2.val) {
-            ListNode temp = l1;
-            temp.next = mergeTwoLists(l1.next, l2);
+        if(l1.val > l2.val) {
+            ListNode temp = new ListNode(l2.val);
+            temp.next = merge(l1, l2.next);
             return temp;
         } else {
-            ListNode temp = l2;
-            temp.next = mergeTwoLists(l1, l2.next);
+            ListNode temp = new ListNode(l1.val);
+            temp.next = merge(l1.next, l2);
             return temp;
         }
     }
