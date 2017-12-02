cee27cf2611e5096bd1f0a8373ee7658d4af561e
pansophism <pansophism@gmail.com>
2017-12-01 18:08:56 -0800

    commiting 169.majority-element.java

--- 169.majority-element.java
+++ 169.majority-element.java
@@ -1,27 +1,18 @@
 class Solution {
+
     public int majorityElement(int[] nums) {
 
-        int candidate = nums[0], count = 1;
+        int count = 0, candidate = -1;
 
-        if(nums.length == 1) {
-            return candidate;
-        }
-
-        for(int i = 1; i < nums.length; i++) {
-
+        for(int i = 0; i < nums.length; i++) {
             if(count == 0) {
-                count++;
                 candidate = nums[i];
+                count++;
             } else {
-                if(nums[i] == candidate) {
-                    count++;
-                } else {
-                    count--;
-                }
+                count--;
             }
         }
 
         return candidate;
     }
 }
-
