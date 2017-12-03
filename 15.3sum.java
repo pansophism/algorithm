ab44606691857751015518e4ed767ecaed5f6363
pansophism <pansophism@gmail.com>
2017-12-03 14:59:09 -0800

    commiting 15.3sum.java

--- 15.3sum.java
+++ 15.3sum.java
@@ -13,7 +13,7 @@ class Solution {
 
         for(int i = 0; i < nums.length - 2; i++) {
 
-            if(i > 0 && nums[i] != nums[i - 1]) {
+            if(i == 0 || nums[i] != nums[i - 1]) {
                 // int target = 0 - nums[i];
 
                 for(int j = i + 1; j < nums.length - 1; j++) {
