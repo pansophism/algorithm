89c5c388bb08f2f1b5da6510539e9efcc5d481c2
pansophism <pansophism@gmail.com>
2017-12-03 12:27:49 -0800

    commiting 493.reverse-pairs.java

--- 493.reverse-pairs.java
+++ 493.reverse-pairs.java
@@ -1,6 +1,4 @@
 
-// find the number of important reverse pairs with the first element of the pair coming from the left subarray nums[0, m] while the second element of the pair coming from the right subarray nums[m + 1, n - 1]
-
 public class Solution {
 
     private int res;
@@ -22,8 +20,6 @@ public class Solution {
         mergeSort(nums, start, mid);
         mergeSort(nums, mid + 1, end);
 
-        // count and merge
-        // count elements: we want nums[left] > 2 * nums[right]
         int count = 0;
         int left = start, right = mid + 1;
         while (left <= mid) {
@@ -35,9 +31,6 @@ public class Solution {
                 count++;
             }
         }
-
-        // merge sort
-        // Arrays.sort(nums, start, end + 1);
 
         left = start; right = mid + 1;
         int index = start;
@@ -55,60 +48,8 @@ public class Solution {
             temp[index++] = nums[right++];
         }
 
-        // copy temp back to nums
         for (index = start; index <= end; index++) {
             nums[index] = temp[index];
         }
     }
 }
-
-// merge sort algorithm
-/*
-public class Solution {
-
-    int[] temp;
-    public void sortIntegers2(int[] A) {
-        // use a shared temp array, the extra memory is O(n) at least
-        temp = new int[A.length];
-        mergeSort(A, 0, A.length - 1);
-    }
-
-    private void mergeSort(int[] A, int start, int end) {
-        if (start >= end) {
-            return;
-        }
-
-        int mid = (start + end) / 2;
-
-        mergeSort(A, start, mid);
-        mergeSort(A, mid + 1, end);
-
-        merge(A, start, mid, end);
-    }
-
-    private void merge(int[] A, int start, int mid, int end) {
-        int left = start;
-        int right = mid + 1;
-        int index = start;
-
-        // merge two sorted subarrays in A to temp array
-        while (left <= mid && right <= end) {
-            if (A[left] < A[right]) {
-                temp[index++] = A[left++];
-            } else {
-                temp[index++] = A[right++];
-            }
-        }
-        while (left <= mid) {
-            temp[index++] = A[left++];
-        }
-        while (right <= end) {
-            temp[index++] = A[right++];
-        }
-
-        // copy temp back to A
-        for (index = start; index <= end; index++) {
-            A[index] = temp[index];
-        }
-    }
-}*/
