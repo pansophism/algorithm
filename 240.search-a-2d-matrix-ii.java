adf55e3f5c6c7f09f43cafd6718c2804d606d8cc
pansophism <pansophism@gmail.com>
2017-12-02 21:24:18 -0800

    commiting 240.search-a-2d-matrix-ii.java

--- 240.search-a-2d-matrix-ii.java
+++ 240.search-a-2d-matrix-ii.java
@@ -4,7 +4,7 @@ class Solution {
 
         int row = 0, col = matrix[0].length - 1;
 
-        while(row < matrix.length  || col >= 0) {
+        while(row < matrix.length  && col >= 0) {
             if(matrix[row][col] > target) {
                 col--;
             } else if(matrix[row][col] < target) {
