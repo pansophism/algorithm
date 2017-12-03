17202fa7d7f6e9644ca883d7ade8b4c1c5392827
pansophism <pansophism@gmail.com>
2017-12-02 20:40:13 -0800

    commiting 312.burst-balloons.java

--- 312.burst-balloons.java
+++ 312.burst-balloons.java
@@ -1,20 +1,21 @@
 class Solution {
 
     public int maxCoins(int[] iNums) {
+
         int [] newArray = new int[iNums.length + 2];
         newArray[0] = 1;
         newArray[newArray.length - 1] = 1;
 
-        for(int i = 1; i <= iNums.length; i++) {
-            newArray[i] = iNums[i - 1];
+        for(int i = 0; i < iNums.length; i++) {
+            newArray[i + 1] = iNums[i];
         }
 
-        int [][] dp = new int[iNums.length + 2][iNums.length + 2];
+        int [][] dp = new int[newArray.length + 1][newArray.length + 1];
 
         return burst(dp, newArray, 0, newArray.length - 1);
     }
 
-    private int burst(int [][] dp, int [] nums, int left, int right) {
+    private int burst(int [][] dp, int [] arr, int left, int right) {
         if(left == right - 1) {
             return 0;
         }
@@ -23,16 +24,15 @@ class Solution {
             return dp[left][right];
         }
 
-        int t = 0;
-
+        int ans = 0;
         for(int i = left + 1; i < right; i++) {
-            t = Math.max(t, nums[left] * nums[i] * nums[right]
-                    + burst(dp, nums, left, i)
-                    + burst(dp, nums, i, right));
+            ans = Math.max(ans, arr[left] * arr[i] * arr[right]
+                    + burst(dp, arr, left, i)
+                    + burst(dp, arr, i, right));
         }
 
-        dp[left][right] = t;
+        dp[left][right] = ans;
 
-        return t;
+        return ans;
     }
 }
