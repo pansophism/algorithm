3aca72bf2ec3fddfe83d8b01e17534caf7b2f8d3
pansophism <pansophism@gmail.com>
2017-12-02 21:25:27 -0800

    commiting 240.search-a-2d-matrix-ii.java

--- 240.search-a-2d-matrix-ii.java
+++ 240.search-a-2d-matrix-ii.java
@@ -2,6 +2,9 @@ class Solution {
 
     public boolean searchMatrix(int[][] matrix, int target) {
 
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
         int row = 0, col = matrix[0].length - 1;
 
         while(row < matrix.length  && col >= 0) {
