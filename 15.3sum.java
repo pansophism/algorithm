763ccf78384e76ac198a282c9e6529e6a3889c62
pansophism <pansophism@gmail.com>
2017-12-03 15:33:50 -0800

    commiting 15.3sum.java

--- 15.3sum.java
+++ 15.3sum.java
@@ -5,40 +5,34 @@ class Solution {
 
         List<List<Integer>> results = new LinkedList<>();
 
-        if(nums == null || nums.length < 3) {
-            return results;
-        }
-
         Arrays.sort(nums);
 
         for(int i = 0; i < nums.length - 2; i++) {
 
-            if(i == 0 || nums[i] != nums[i - 1]) {
-                int target = 0 - nums[i];
+            if(i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
 
-                int l = i + 1, r = nums.length - 1;
+            int target = 0 - nums[i];
 
-                while(l < r) {
-                    if(nums[l] + nums[r] > target) {
-                        r--;
-                    } else if(nums[l] + nums[r] < target) {
+            int l = i + 1, r = nums.length - 1;
+
+            while(l < r) {
+                if(nums[l] + nums[r] == target) {
+                    results.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    l++;
+                    r--;
+
+                    while(l < r && nums[l] == nums[l - 1]) {
                         l++;
-                    } else {
-                        List<Integer> oneEntry = new LinkedList<>();
-                        oneEntry.add(nums[i]);
-                        oneEntry.add(nums[l]);
-                        oneEntry.add(nums[r]);
-                        results.add(oneEntry);
-                        r++;
-                        l--;
-
-                        while(l < r && nums[l] == nums[l - 1]) {
-                            l++;
-                        }
-                        while(l < r && r < nums.length - 1 && nums[r] == nums[r + 1]) {
-                            r--;
-                        }
                     }
+                    while(l < r && r < nums.length - 1 && nums[r] == nums[r + 1]) {
+                        r--;
+                    }
+                } else if(nums[l] + nums[r] > target) {
+                    r--;
+                } else if(nums[l] + nums[r] < target) {
+                    l++;
                 }
             }
         }
