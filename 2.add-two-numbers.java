dc91f9908c1a8d3ab7b505450720fa9fcc7cad4c
pansophism <pansophism@gmail.com>
2017-12-03 16:18:34 -0800

    commiting 2.add-two-numbers.java

--- 2.add-two-numbers.java
+++ 2.add-two-numbers.java
@@ -8,6 +8,61 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        
+        ListNode newHead = new ListNode(0);
+        ListNode pointer = newHead;
+
+        int carry = 0;
+
+        while(l1 != null && l2 != null) {
+            int v = l1.val + l2.val + carry;
+            l1 = l1.next;
+            l2 = l2.next;
+
+            if(v >= 10) {
+                carry = 1;
+            } else {
+                carry = 0;
+            }
+
+            ListNode temp = new ListNode(v >= 10 ? v - 10 : v);
+            pointer.next = temp;
+            pointer = pointer.next;
+        }
+
+        while(l1 != null) {
+            int v = l1.val + carry;
+            l1 = l1.next;
+            if(v >= 10) {
+                carry = 1;
+            } else {
+                carry = 0;
+            }
+
+            ListNode temp = new ListNode(v >= 10 ? v - 10 : v);
+            pointer.next = temp;
+            pointer = pointer.next;
+        }
+
+        while(l2 != null) {
+            int v = l2.val + carry;
+            l2 = l2.next;
+
+            if(v >= 10) {
+                carry = 1;
+            } else {
+                carry = 0;
+            }
+
+            ListNode temp = new ListNode(v >= 10 ? v - 10 : v);
+            pointer.next = temp;
+            pointer = pointer.next;
+        }
+
+        if(carry == 1) {
+            ListNode temp = new ListNode(1);
+            pointer.next = temp;
+        }
+
+        return newHead.next;
     }
 }
