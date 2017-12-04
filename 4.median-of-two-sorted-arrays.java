ba3466823a5400dfb66f928e4a15e8a63b487789
pansophism <pansophism@gmail.com>
2017-12-03 19:40:31 -0800

    commiting 4.median-of-two-sorted-arrays.java

--- 4.median-of-two-sorted-arrays.java
+++ 4.median-of-two-sorted-arrays.java
@@ -1,52 +1,47 @@
 class Solution {
 
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int len1 = nums1.length, len2 = nums2.length;
+        int l1 = nums1.length, l2 = nums2.length;
 
-        if(((len1 + len2) & 1) == 1) {
-            // if odd number, pick the mid
-            return findKth(nums1, 0, len1, nums2, 0, len2, (len1 + len2) / 2 + 1);
-        } else {
-            return
-                // if even number, pick the average mid and mid + 1
-                (findKth(nums1, 0, len1, nums2, 0, len2, (len1 + len2) / 2) +
-                 findKth(nums1, 0, len1, nums2, 0, len2, (len1 + len2) / 2 + 1)) / 2;
+        int k = (l1 + l2) / 2;
+
+        if((l1 + l2) % 2 == 1) {
+            return findKth(nums1, 0, l1, nums2, 0, l2, k + 1);
         }
+
+        return (findKth(nums1, 0, l1, nums2, 0, l2, k)
+                + findKth(nums1, 0, l1, nums2, 0, l2, k + 1)) / 2;
     }
 
-    private double findKth(int [] nums1, int start1, int end1,
-            int [] nums2, int start2, int end2,
+    private double findKth(
+            int [] nums1, int lo1, int hi1,
+            int [] nums2, int lo2, int hi2,
             int k) {
 
-        int len1 = end1 - start1;
-        int len2 = end2 - start2;
+        int m1 = lo1 + (hi1 - lo1) / 2;
+        int m2 = lo2 + (hi2 - lo2) / 2;
 
-        if(len1 <= 0) return nums2[start2 + k - 1];
-        if(len2 <= 0) return nums1[start1 + k - 1];
+        if(lo1 >= hi1) return nums2[lo2 + k - 1];
+        if(lo2 >= hi2) return nums1[lo1 + k - 1];
 
-        if(k <= 1) return Math.min(nums1[start1], nums2[start2]);
+        if(k <= 1) return Math.min(nums1[lo1], nums2[lo2]);
 
-        int mid1 = start1 + (end1 - start1) / 2;
-        int mid2 = start2 + (end2 - start2) / 2;
+        int len1 = hi1 - lo1;
+        int len2 = hi2 - lo2;
 
-        if(len1 / 2 + len2 / 2 + 1 >= k) { // k is on left side
-
-            if(nums1[mid1] >= nums2[mid2]) { // nums1 is bigger, discard right side of nums1
-                return findKth(nums1, start1, mid1, nums2, start2, end2, k);
-            } else { // nums2 if bigger, discard right side of nums2
-                return findKth(nums1, start1, end1, nums2, start2, mid2, k);
+        if(len1 / 2 + len2 / 2 + 1 >= k) {
+            if(nums1[m1] >= nums2[m2]) {
+                return findKth(nums1, lo1, m1, nums2, lo2, hi2, k);
+            } else {
+                return findKth(nums1, lo1, hi1, nums2, lo2, m2, k);
             }
-
-        } else { // k is on right side
-
-            if(nums1[mid1] >= nums2[mid2]) { // nums1 is bigger, discard left side of nums2
-                return findKth(nums1, start1, end1, nums2, mid2 + 1, end2, k - (len2 / 2 + 1));
-            } else { // nums2 if bigger, discard left side of nums1
-                return findKth(nums1, mid1 + 1, end1, nums2, start2, end2, k - (len1 / 2 + 1));
+        } else {
+            if(nums1[m1] >= nums2[m2]) {
+                return findKth(nums1, lo1, hi1, nums2, m2 + 1, hi2, k - (len2 / 2 + 1));
+            } else {
+                return findKth(nums1, m1 + 1, hi1, nums2, lo2, hi2, k - (len1 / 2 + 1));
             }
         }
+
     }
-
 }
-
-
