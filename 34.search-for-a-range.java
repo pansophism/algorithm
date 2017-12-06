8de765d8f5002fcde6f745fec45a3693084f3493
pansophism <pansophism@gmail.com>
2017-12-06 01:33:02 -0800

    commiting 34.search-for-a-range.java

--- 34.search-for-a-range.java
+++ 34.search-for-a-range.java
@@ -2,60 +2,64 @@
 class Solution {
 
     public int[] searchRange(int[] nums, int target) {
-        int [] res = new int[2];
-        if(nums == null || nums.length < 1) {
-            res[0] = -1;
-            res[1] = -1;
-            return res;
-        }
 
-        int start = findFirstGreater(nums, target - 1);
-        if(start == nums.length || nums[start] != target) {
+        if(nums.length == 0) {
             return new int[]{-1, -1};
         }
 
-        int end = findFirstGreater(nums, target + 1) - 1;
-        if(start == end - 1) {
-            end = start;
-        }
-        return new int[] {start, end};
-    }
+        int start, end, mid;
 
-    public int findFirstGreater(int [] nums, int target) {
-        int lo = 0, hi = nums.length - 1;
+        int [] bound = new int[2];
 
-        while(lo + 1 < hi) {
+        // search for left bound
+        start = 0;
+        end = nums.length - 1;
 
-            int mid = lo + (hi - lo) / 2;
+        while(start + 1 < end) {
+            mid = start + (end - start) / 2;
 
-            if(nums[mid] <= target) {
-                lo = mid;
+            if(nums[mid] >= target) {
+                end = mid;
             } else {
-                hi = mid;
+                start = mid;
             }
         }
 
-        if(nums[lo] > target) {
-            return lo;
+        if(nums[start] == target) {
+            bound[0] = start;
+        } else if(nums[end] == target) {
+            bound[0] = end;
+        } else {
+            bound[0] = bound[1] = -1;
+            return bound;
         }
 
-        if(nums[hi] < target) {
-            return hi + 1;
+        // search for right bound
+
+        start = 0;
+        end = nums.length - 1;
+
+        while(start + 1 < end) {
+            mid = start + (end - start) / 2;
+
+            if(nums[mid] <= target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
         }
 
-        return hi;
-    }
+        if(nums[end] == target) {
+            bound[1] = end;
+        } else if(nums[start] == target) {
+            bound[1] = start;
+        } else {
+            bound[0] = bound[1] = -1;
+            return bound;
+        }
 
-    public static void main(String [] args) {
-        Solution s = new Solution();
+        return bound;
 
-        int [] d = {5,7,7,8,8,10};
-
-        System.out.println("first greater 0 : " + s.findFirstGreater(d, 0));
-        System.out.println("first greater 7 : " + s.findFirstGreater(d, 7));
-        System.out.println("first greater 8 : " + s.findFirstGreater(d, 8));
-        System.out.println("first greater 9 : " + s.findFirstGreater(d, 9));
-        System.out.println("first greater 10 : " + s.findFirstGreater(d, 10));
 
     }
 }
