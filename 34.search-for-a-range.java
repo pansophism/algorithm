eeb05f9e79860c10520ef96871dd80baaa3b5252
pansophism <pansophism@gmail.com>
2017-12-06 00:13:06 -0800

    commiting 34.search-for-a-range.java

--- 34.search-for-a-range.java
+++ 34.search-for-a-range.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         int [] res = new int[2];
-        if(nums == null || nums.length < 2) {
+        if(nums == null || nums.length < 1) {
             res[0] = -1;
             res[1] = -1;
             return res;
         }
 
-        res[0] = findLastSmaller(nums, target) + 1;
+        res[0] = findLastSmaller(nums, target);
         res[1] = findLastSmaller(nums, target + 1);
 
         return res;
@@ -15,6 +15,14 @@ class Solution {
 
     private int findLastSmaller(int [] nums, int target) {
         int lo = 0, hi = nums.length - 1;
+
+        if(nums[lo] <= target) {
+            return lo;
+        }
+
+        if(nums[hi] < target) {
+            return hi + 1;
+        }
 
         while(lo + 1 < hi) {
             int mid = lo + (hi - lo) / 2;
